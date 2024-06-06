e582e0916cfb8ead40d8da334a8c5c942e238715
Carl <carl.mikaelsson@chasacademy.se>
2024-06-06 23:34:05 +0200

    u

---
+++ pokemon-backend/pages/api/auth/signup.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+
+const prisma = new PrismaClient();
+
+export default async function handler(req: NextApiRequest, res: NextApiRequest) {
+  if (req.method === 'POST') {
+    try {
+      const { email, name, password } = req.body;
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      const newUser = await prisma.user.create({
+        data: { email, name, password: hashedPassword },
+      });
+
+      const token = jwt.sign({ userId: newUser.id }, process.env.JWT_SECRET, {
+        expiresIn: '1h',
+      });
+
+      res.status(201).json({ token });
+    } catch (error) {
+      res.status(500).json({ message: 'Internal Server Error' });
+    }
+  } else {
+    res.status(405).json({ message: 'Method not allowed' });
+  }
+}
