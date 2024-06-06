628008e97ea35a08903196b960ff906f912d4d51
Carl <carl.mikaelsson@chasacademy.se>
2024-06-06 23:41:13 +0200

    a

--- pokemon-backend/pages/api/auth/signup.ts
+++ pokemon-backend/pages/api/auth/signup.ts
@@ -3,10 +3,9 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-
 const prisma = new PrismaClient();
 
-export default async function handler(req: NextApiRequest, res: NextApiRequest) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     try {
       const { email, name, password } = req.body;
@@ -16,7 +15,7 @@ export default async function handler(req: NextApiRequest, res: NextApiRequest) 
         data: { email, name, password: hashedPassword },
       });
 
-      const token = jwt.sign({ userId: newUser.id }, process.env.JWT_SECRET, {
+      const token = jwt.sign({ userId: newUser.id }, process.env.JWT_SECRET as string, {
         expiresIn: '1h',
       });
 
--- pokemon-backend/pages/api/users/index.ts
+++ pokemon-backend/pages/api/users/index.ts
@@ -9,8 +9,8 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const users = await prisma.user.findMany({ include: { pokemons: true } });
     res.json(users);
   } else if (req.method === 'POST') {
-    const { email, name } = req.body;
-    const newUser = await prisma.user.create({ data: { email, name } });
+    const { email, name, password } = req.body;
+    const newUser = await prisma.user.create({ data: { email, name, password } });
     res.json(newUser);
   } else {
     res.status(405).json({ message: 'Method not allowed' });
